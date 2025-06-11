6b7c093d8fc4bae28c943dfce4f610ee5771b6f7
Joe simpson <joe@egmrc.com>
2025-06-11 16:16:10 -0600

    Update Form component: add `justify-between` for better field spacing

--- src/blocks/Form/Component.tsx
+++ src/blocks/Form/Component.tsx
@@ -137,7 +137,7 @@ export const FormBlock: React.FC<
         {error && <div>{`${error.status || '500'}: ${error.message || ''}`}</div>}
         {!hasSubmitted && (
           <form id={formID} onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-4 last:mb-0 flex flex-wrap">
+            <div className="mb-4 last:mb-0 flex flex-wrap justify-between">
               {formFromProps &&
                 formFromProps.fields &&
                 formFromProps.fields?.map((field, index) => {
